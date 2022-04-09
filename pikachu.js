5c37baec526b6026af8ca235603a670c0f7430ba
7MinutesDead-Git <7minutesdead@gmail.com>
2022-04-09 18:03:39 -0500

    initial commit

---
+++ pikachu.js
@@ -0,0 +1,77 @@
+const pokeAPI = 'https://pokeapi.co/api/v2/'
+const searchType = 'pokemon/'
+
+function renderPokemon(json) {
+    const spriteTypes = json['sprites']
+    for (const sprite in spriteTypes) {
+        const url = spriteTypes[sprite]
+        if (typeof url === 'string' && url.startsWith('https')) {
+            const newImg = document.createElement("img")
+            newImg.src = url
+            document.querySelector('.pikachu-container').appendChild(newImg)
+            console.log(url)
+        }
+    }
+}
+function getPokemon(reqURL) {
+    fetch(reqURL)
+        .then(res => res.json())
+        .then(json => {
+            console.log(json)
+            renderPokemon(json)
+        })
+        .catch(error => {
+            console.log(`Error when fetching: ${error}`)
+        })
+}
+
+function revealNewPokemon() {
+    document.querySelectorAll('img').forEach(img => img.style.opacity = '1')
+}
+
+function getAndRevealPokemon() {
+    const searchTarget = document.querySelector('#pokesearch').value
+    console.log(searchTarget)
+    const pokeURL = `${pokeAPI}${searchType}${searchTarget}`
+    getPokemon(pokeURL)
+    setTimeout(revealNewPokemon, 50)
+}
+
+function flashElements(...elements) {
+    for (const element of elements) {
+        element.classList.add('flicker')
+        setTimeout(() => {
+            element.classList.remove('flicker')
+        }, 200)
+    }
+}
+
+function destroyPokemon() {
+    const allImages = document.querySelectorAll('img')
+    for (const img of allImages) {
+        img.remove()
+    }
+}
+
+window.onload = () => {
+    const searchButton = document.querySelector('#pokesearch-button')
+    const searchInput = document.querySelector('#pokesearch')
+    const clearButton = document.querySelector('#clear')
+
+    searchButton.addEventListener('click', () => {
+        getAndRevealPokemon()
+        flashElements(searchButton, searchInput, document.body)
+    })
+    // Also submit on hitting enter.
+    searchInput.addEventListener('keyup', e => {
+        if (e.keyCode === 13) {
+            getAndRevealPokemon()
+            flashElements(searchButton, searchInput, document.body)
+        }
+    })
+
+    clearButton.addEventListener('click', () => {
+        destroyPokemon()
+        flashElements(document.body, clearButton)
+    })
+}
