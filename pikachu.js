fe2bec875036ffbfed5c11d05e53fd7f548ff764
7MinutesDead-Git <7minutesdead@gmail.com>
2022-04-09 21:39:08 -0500

    - Added pokemon bloating on click. - Added error message feedback. - Added loading icon when waiting for API response.

--- pikachu.js
+++ pikachu.js
@@ -2,14 +2,30 @@ const pokeAPI = 'https://pokeapi.co/api/v2/'
 const searchType = 'pokemon/'
 
 function renderPokemon(json) {
+    const container = document.querySelector('.pikachu-container')
     const spriteTypes = json['sprites']
+    let previousPokemon = null
+
     for (const sprite in spriteTypes) {
         const url = spriteTypes[sprite]
+
         if (typeof url === 'string' && url.startsWith('https')) {
-            const newImg = document.createElement("img")
-            newImg.src = url
-            document.querySelector('.pikachu-container').appendChild(newImg)
-            console.log(url)
+            const pokemon = document.createElement("img")
+            pokemon.src = url
+
+            // Don't ask me why yet.
+            pokemon.addEventListener('click', (e) => {
+                const currentSize = parseInt(getComputedStyle(e.target).width)
+                e.target.style.width = `${currentSize + 90}px`
+                e.target.style.height = `${currentSize + 90}px`
+            })
+            container.appendChild(pokemon)
+            // // If shifting new pokemon to the front is desired, uncomment.
+            // if (!previousPokemon)
+            //     container.appendChild(pokemon)
+            // else
+            //     container.insertBefore(pokemon, previousPokemon)
+            // previousPokemon = document.querySelector('img:first-of-type')
         }
     }
 }
@@ -22,10 +38,14 @@ function getPokemon(reqURL) {
         })
         .then(() => {
             // A slight delay so the animation has time to play.
-            setTimeout(revealNewPokemon, 50)
+            toggleLoadingIcon()
+            setTimeout(revealNewPokemon, 30)
         })
         .catch(error => {
-            console.log(`Error when fetching: ${error}`)
+            const errorElement = document.querySelector('span.error-text')
+            errorElement.innerText = `${error}`
+            flashElements(errorElement)
+            toggleLoadingIcon()
         })
 }
 
@@ -35,9 +55,17 @@ function revealNewPokemon() {
 
 function getAndRevealPokemon() {
     const searchTarget = document.querySelector('#pokesearch').value.toLowerCase().trim()
-    console.log(searchTarget)
-    const pokeURL = `${pokeAPI}${searchType}${searchTarget}`
-    getPokemon(pokeURL)
+    if (searchTarget.length > 0) {
+        console.log(searchTarget)
+        const pokeURL = `${pokeAPI}${searchType}${searchTarget}`
+        getPokemon(pokeURL)
+    }
+    else {
+        const errorElement = document.querySelector('span.error-text')
+        errorElement.innerText = "You gotta enter something!"
+        flashElements(errorElement)
+        toggleLoadingIcon()
+    }
 }
 
 function flashElements(...elements) {
@@ -52,8 +80,13 @@ function flashElements(...elements) {
 function destroyPokemon() {
     const allImages = document.querySelectorAll('img')
     for (const img of allImages) {
-        img.remove()
+        img.style.opacity = "0"
+        setTimeout(() => img.remove(), 500)
     }
+}
+
+function toggleLoadingIcon() {
+    document.querySelector('.lds-roller').classList.toggle('reveal')
 }
 
 window.onload = () => {
@@ -62,12 +95,14 @@ window.onload = () => {
     const clearButton = document.querySelector('#clear')
 
     searchButton.addEventListener('click', () => {
+        toggleLoadingIcon()
         getAndRevealPokemon()
         flashElements(searchButton, searchInput, document.body)
     })
     // Also submit on hitting enter.
     searchInput.addEventListener('keyup', e => {
         if (e.keyCode === 13) {
+            toggleLoadingIcon()
             getAndRevealPokemon()
             flashElements(searchButton, searchInput, document.body)
         }
